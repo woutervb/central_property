b5f387a846daadf80489a7fcedd7ec6a7ba371a7
Wouter van Bommel <wouter@vanbommelonline.nl>
2012-06-17 14:42:56 +0200

    Split of url handling

--- central_property/urls.py
+++ central_property/urls.py
@@ -14,6 +14,5 @@ urlpatterns = patterns('',
 
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^store/$', 'store.views.index'),
-    url(r'^store/(?P<object_ref>[\w\/]+)', 'store.views.store'),
+    url(r'^store/', include('store.urls')),
 )
---
+++ store/urls.py
@@ -0,0 +1,6 @@
+from django.conf.urls import patterns, include, url
+
+urlpatterns = patterns('store.views',
+    url(r'^$', 'index'),
+    url(r'^(?P<object_ref>[\w\/]+)', 'store'),
+)
