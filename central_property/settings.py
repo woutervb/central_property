bbc80b8d6f72ba9973d4c5f62ded1dad600dc092
Wouter van Bommel <wouter@vanbommelonline.nl>
2012-06-20 20:18:40 +0200

    Some more infrastructure elements.

--- central_property/settings.py
+++ central_property/settings.py
@@ -112,7 +112,7 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    
+    'C:/Users/bommel/repositories/central_property',
 )
 
 # This variable is introduced as we need to append it for the admin pages of django-treebeard
