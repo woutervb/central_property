93784c16f75d8944d280abed66e626d6093c8a5e
Wouter van Bommel <wouter@vanbommelonline.nl>
2012-07-02 12:39:03 +0200

    Added a backport of django-treebeard 2.0 to the admin pages Works relatively well. The drawback is that the project now depends on Django 1.4 for proper working.

--- store/admin.py
+++ store/admin.py
@@ -1,18 +1,25 @@
 from store.models import KeyValue, Tree
 from django.contrib import admin
-from treebeard.admin import TreeAdmin
+from forms import MoveNodeForm
 
+class TreeAdmin(admin.ModelAdmin):
+    "Django Admin class for treebeard"
+    change_list_template = 'admin/tree_change_list.html'
+    form = MoveNodeForm
+
+    def queryset(self, request):
+        from treebeard.al_tree import AL_Node
+        if issubclass(self.model, AL_Node):
+            # AL Trees return a list instead of a QuerySet for .get_tree()
+            # So we're returning the regular .queryset cause we will use
+            # the old admin
+            return super(TreeAdmin, self).queryset(request)
+        else:
+            return self.model.get_tree()
 
 class KeyValueInline(admin.StackedInline):
     model = KeyValue.tree_id.through
     extra = 3
-
-#class ParentAdmin(admin.ModelAdmin):
-#    change_list_template = 'admin/tree_change_list.html'
-class ParentAdmin(admin.ModelAdmin):
-    search_fields = ('name', )
-    change_list_template = 'admin/tree_change.html'
-    inlines = [KeyValueInline]
 
 class KeyValueAdmin(admin.ModelAdmin):
     fields = (('key', 'value'),)
